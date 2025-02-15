8e7eb2e560d29687c1823456209706ee789bfb41
idkmanLOL63 <vo123quan@gmail.com>
2025-02-15 12:14:44 +0700

    Update Chams ESP.lua

--- Chams ESP.lua
+++ Chams ESP.lua
@@ -3,22 +3,78 @@ getgenv().esp = {
     FillColor = Color3.fromRGB(255, 255, 0)
     OutlineColor = Color3.fromRGB(0, 0, 0)
 }
-local function highlightPlayer(player)
-    if player.Character then
-        local highlight = Instance.new("Highlight")
-        highlight.Parent = player.Character
-        highlight.FillColor = Color3.fromRGB(255, 255, 0)
-        highlight.OutlineColor = Color3.fromRGB(0, 0, 0)
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
+
+local ChamsAdjustments = {
+ Enabled = false,
+ OutlineColor = Color3.fromRGB(255, 255, 255),
+ OutlineTransparency = 0,
+ FillColor = Color3.fromRGB(120, 255, 0),
+ FillTransparency = 0,
+ 
+ Keybind = Enum.KeyCode.Z,
+}
+
+local function AddChams(v)
+ if v and v.Character and not v.Character:FindFirstChild(v.Name.."_Chams") then
+  local ChamsESP = Instance.new("Highlight")
+  
+  ChamsESP.Name = v.Name.."_Chams"
+  
+  ChamsESP.OutlineColor = ChamsAdjustments.OutlineColor
+  ChamsESP.OutlineTransparency = ChamsAdjustments.OutlineTransparency
+  ChamsESP.FillColor = ChamsAdjustments.FillColor
+  ChamsESP.FillTransparency = ChamsAdjustments.FillTransparency
+  
+  ChamsESP.Parent = v.Character
+  ChamsESP.Adornee = v.Character
+ end
+end
+
+local function UpdateChams(v)
+ if v and v.Character and v.Character:FindFirstChild(v.Name.."_Chams") then
+  local ChamsESP = v.Character:FindFirstChild(v.Name.."_Chams")
+  ChamsESP.Enabled = ChamsAdjustments.Enabled
+  ChamsESP.OutlineColor = ChamsAdjustments.OutlineColor
+  ChamsESP.OutlineTransparency = ChamsAdjustments.OutlineTransparency
+  ChamsESP.FillColor = ChamsAdjustments.FillColor
+  ChamsESP.FillTransparency = ChamsAdjustments.FillTransparency
+ end
+end
+
+local function RemoveChams(v)
+ if v and v.Character and v.Character:FindFirstChild(v.Name.."_Chams") then
+  v.Character:FindFirstChild(v.Name.."_Chams"):Destroy()
+ end
+end
+
+UserInputService.InputBegan:Connect(function(Key)
+ if Key.KeyCode == ChamsAdjustments.Keybind and UserInputService:GetFocusedTextBox() == nil then
+  ChamsAdjustments.Enabled = not ChamsAdjustments.Enabled
+ end
+end)
+
+RunService.Stepped:Connect(function()
+ if ChamsAdjustments.Enabled == true then
+  for _,v in pairs(Players:GetPlayers()) do
+   if v ~= LocalPlayer and v.Character then
+    if not v.Character:FindFirstChild(v.Name.."_Chams") then
+     AddChams(v)
+    elseif v.Character:FindFirstChild(v.Name.."_Chams") then
+     UpdateChams(v)
     end
-end
-
-
-for _, player in pairs(game.Players:GetPlayers()) do
-    highlightPlayer(player)
-end
-
-game.Players.PlayerAdded:Connect(function(player)
-    player.CharacterAdded:Connect(function()
-        highlightPlayer(player)
-    end)
+   end
+  end
+ elseif ChamsAdjustments.Enabled == false then
+  for _,v in pairs(Players:GetPlayers()) do
+   if v ~= LocalPlayer and v.Character then
+    if v.Character:FindFirstChild(v.Name.."_Chams") then
+     RemoveChams(v)
+    end
+   end
+  end
+ end
 end)
